7915653a4042fd03a658bcc32af6c14fd40fcb4d
zapwerks <chris.streich@gmail.com>
2016-07-27 12:55:23 +0000

    final

--- .~c9_invoke_nnUghk.py
+++ .~c9_invoke_nnUghk.py
@@ -22,32 +22,25 @@ ingredients = {
 
 
 def find_preference():
-    """This function asks the customer for their drink preferences"""
     preferences = {} 
     for taste, question in questions.items():
+        
         print(question)
         preferences[taste] = input() in ["y", "yes"]
     return preferences    
     
-
+def make_drink(pref):
 def make_drink(preferences):
-    """This function takes the output of the Preference Function and passes it
-    into the Drinks function. It then uses random to make a random combination 
-    for the customer"""
     drink = []
     for key, value in preferences.items():
         if value is True:
             drink.append(random.choice(ingredients[key]))
     return drink
 
-
-  
-def main():
-    results = find_preference()
-    print("\nGreat! Based on your choices, I recommend a cocktail with a:")
-    for drink in make_drink(results):
-        print("    " + drink)
+def main()
+    preference = find_preference()
+    drink = make_drink()
 
 
-if __name__ == "__main__":
-    main() 
+#if __name__ == "__main__":
+#   main()
